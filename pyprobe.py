04c8a186f9cf32b29dce1255e01738b7de0ca2d9
Anush <anushvenkat101@gmail.com>
2020-03-03 14:50:49 +0000

    Added monitoring capability

--- pyprobe.py
+++ pyprobe.py
@@ -1,10 +1,17 @@
 import socket
 import getmac
 from tabulate import tabulate
+import time
+import argparse
+
+parser = argparse.ArgumentParser(description='Sniff out machines on local network')
+parser.add_argument('-m', '--monitor', help='monitor continuously', action='store_true')
+args = parser.parse_args()
 
 macs = []
 
 def probe(variant):
+
     for i in range (0, 255):
         probe_dest = variant + str(i)
         hostname = socket.gethostbyaddr(probe_dest)[0]
@@ -14,8 +21,19 @@ def probe(variant):
             mac_address = getmac.get_mac_address(ip=probe_dest, network_request=True)
             macs.append([probe_dest, 'Default-Gateway' if hostname == '_gateway' else hostname, mac_address if mac_address is not None else 'local-machine'])
 
-probe('192.168.0.')
-probe('192.168.1.')
 
-print(tabulate(macs, headers=['Local-ip','Hostname', 'MacID']))
-print('\nNo. of connected devices: ' + str(len(macs) - 1))
+def print_probe():
+    print(tabulate(macs, headers=['Local-ip','Hostname', 'MacID']))
+    print('\nNo. of connected devices: ' + str(len(macs) - 1) + '\n')
+
+if args.monitor:
+    while True:
+        probe('192.168.0.')
+        probe('192.168.1.')
+        print_probe()
+        time.sleep(1)
+        macs.clear()
+else:
+    probe('192.168.0.')
+    probe('192.168.1.')
+    print_probe()
